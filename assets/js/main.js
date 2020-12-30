09759134a2ecd5d95d4380fd82b957d7baccb3d5
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2020-12-30 10:20:59 +0530

    Update skills and add transitions

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll('.nav_link');   
 
 function linkAction(){
   /*Active link*/
@@ -33,24 +33,24 @@ const sr = ScrollReveal({
 });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
+sr.reveal('.home_title',{}); 
 sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.home_img',{delay: 400}); 
+sr.reveal('.home_social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about_img',{}); 
+sr.reveal('.about_subtitle',{delay: 400}); 
+sr.reveal('.about_text',{delay: 400}); 
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills_subtitle',{}); 
+sr.reveal('.skills_text',{}); 
+sr.reveal('.skills_data',{interval: 200}); 
+sr.reveal('.skills_img',{delay: 600});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal('.work_img',{interval: 200}); 
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal('.contact_input',{interval: 200}); 
